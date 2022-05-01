13e41f91a38c315be48ec4ad33138e1a13db2ddb
Simeon Martev <simeon.martev@adata.pro>
2022-05-01 22:09:26 +0300

    Update json schema

--- crawl_licitatie/crawl_licitatie/items.py
+++ crawl_licitatie/crawl_licitatie/items.py
@@ -1,29 +1,30 @@
 from scrapy_jsonschema.item import JsonSchemaItem
 
 
-class CrawlLicitatieItem(JsonSchemaItem):
+class CrawlLicitatieNotice(JsonSchemaItem):
     jsonschema = {
         "$schema": "http://json-schema.org/draft-07/schema",
-        "title": "licitatie-object",
-        "description": "A licitatie crawl object",
+        "title": "licitatie-notice",
+        "description": "A licitatie crawl notice",
         "type": "object",
         "properties": {
+            "notice_id": {"type": "string"},
             "notice_number": {"type": "string"},
             "tender_name": {"type": "string"},
-            "procedure_status": {
-                "enum": [
-                    "Published",
-                ]
-            },
-            "contract_type": {
-                "enum": [
-                    "Services",
-                    "Supply",
-                    "In desfasurare",
-                ]
-            },
+            "procedure_status": {"type": "string"},
+            "contract_type": {"type": "string"},
             "type_of_procurement": {"type": "string"},
-            "estimated_value": {"type": "string"},
-            "date": {"type": "string", "format": "date-time"},
+            "estimated_value": {"type": "number", "minimum": 0},
+            "datetime": {"type": "string", "format": "date-time"},
         },
+        "required": [
+            "notice_id",
+            "notice_number",
+            "tender_name",
+            "procedure_status",
+            "contract_type",
+            "type_of_procurement",
+            "estimated_value",
+            "datetime",
+        ],
     }
