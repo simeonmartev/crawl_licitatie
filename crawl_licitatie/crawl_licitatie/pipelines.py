dd1218b61f14a294f3e75b5b248b37ad19d0dc79
Simeon Martev <simeon.martev@adata.pro>
2022-06-27 15:23:53 +0300

    Update poetry

--- crawl_licitatie/crawl_licitatie/pipelines.py
+++ crawl_licitatie/crawl_licitatie/pipelines.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 class CrawlLicitatiePipeline:
     def process_item(self, item, spider) -> NoticeModel:
-        __import__("ipdb").set_trace()
+        # __import__("ipdb").set_trace()
         notice = NoticeModel.create(**item)
         pprint(notice)
         return notice
